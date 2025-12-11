abd562833a4f6e1251274296a0dd8b9ae9585390
Sam Warr <sam.warr.tx@gmail.com>
2025-12-11 16:12:02 -0700

    failed build, rip

--- OpenCalc.cpp
+++ OpenCalc.cpp
@@ -6,4 +6,14 @@ int main()
 {
     stdio_init_all();
     set_sys_clock_khz(200000, true);
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed\n");
+        return -1;
+    }
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+
+    while (true) {
+        printf("Hello, OpenCalc!\n");
+        sleep_ms(1000);
+    }
 }
